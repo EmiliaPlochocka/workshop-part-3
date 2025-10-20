458b0501dfb6f89083d6bb196bcf39090dadb2eb
EmiliaPlochocka <paradoksalnie.emu@gmail.com>
2025-10-20 16:12:15 +0200

    added grouping, export, password age

--- ad_audit.ps1
+++ ad_audit.ps1
@@ -7,7 +7,7 @@ $jsonText = Get-Content -Path $jsonPath -Raw -Encoding UTF8
 # Converting json to PowerShell using ConvertFrom
 $data = $jsonText | ConvertFrom-Json
 
-#___DISPLAY DOMAIN NAME & EXPORT DATA___
+#___DISPLAY DOMAIN NAME & EXPORT DATE___
 # retrieve info from JSON: domain and export_date
 Write-Host "Domain: $($data.domain)"
 Write-Host "Export Date: $($data.export_date)"
@@ -25,6 +25,7 @@ foreach ($user in $inactiveUsers) {
     $daysInactive = ((Get-Date) - [datetime]$user.lastLogon).Days
     Write-Host "$($user.samAccountName)`t$($user.displayName)`t$daysInactive days"
 }
+$inactiveUsers | Select-Object samAccountName, displayName, lastLogon | Export-Csv -Path "inactive_users.csv"
 
 #___NO. USERS PER DEPARTMENT___
 # create empty counter
@@ -42,3 +43,25 @@ Write-Host "`n Users per department:"
 foreach ($dept in $departmentCount.Keys) {
     Write-Host "$dept :$($departmentCount[$dept]) users"
 }
+
+#___GROUPING COMPUTERS___
+# grouping computers by the 'site' value in json
+$groupedComputers = $data.computers | Group-Object -Property site
+
+foreach ($group in $groupedComputers) {
+    Write-Host "$($group.Name) has $($group.Count) computers"
+}
+
+# ___PASSWORD AGE___
+$data.users  | ForEach-object {
+    $passwordAge = (Get-Date) - [datetime]$_.passwordLastSet
+    [PSCustomObject]@{
+        SamAccountName  = $_.samAccountName
+        DisplayName     = $_.displayName
+        PasswordAgeDays = [math]::Round($passwordAge.TotalDays, 0)
+    }
+} | Format-Table
+
+#___OLDEST LOGIN___
+$top10OldCOmputers = $data.computers | Sort-Object { [datetime]._.lastLogon } | Select-Object -First 10
+$top10OldCOmputers | Select-Object name, site, lastLogon | Format-Table
